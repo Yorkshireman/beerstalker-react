c308643a98df6b98eaf6867c3accc75e9554909f
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2019-03-22 17:13:38 +0000

    API request working and api key grabbed using dotenv

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const request = require('request');
+
+require('dotenv').config();
 
 const app = express();
 
@@ -8,59 +11,38 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
-app.get('/', (req, res) => {
-  res.send('HELLO!!');
+app.get('/', ({ query: { city } }, res) => {
+  const meetupApiKey = process.env.MEETUP_API_KEY;
+  const path = `https://api.meetup.com/2/open_events.json?and_text=true&text=free+beer&country=gb&city=${city}&key=${meetupApiKey}&text_format=plain&order=distance`;
+  request(path, (error, response, body) => {
+    console.log('error:', error);
+    console.log('statusCode:', response && response.statusCode);
+
+    const { results } = JSON.parse(body);
+    const filteredResults = [];
+
+    for (index = 0; index < results.length; index++) {
+      const result = results[index].description;
+
+      try {
+        result.indexOf('free beer');
+      } catch {
+        break;
+      }
+
+      if (result.indexOf('free beer') >= 0) {
+        filteredResults.push(results[index]);
+      }
+    }
+
+    console.log('============== start ================');
+    console.log(JSON.stringify(filteredResults, null, 2));
+    console.log('=============== end ===============');
+
+    res.send(filteredResults);
+  });
 });
 
 app.listen(app.get('port'), () => {
   console.log(`Listening on port http://localhost:${app.get('port')}`);
 });
-
-
-
-
-// customSearch: function(cityName) {
-//   var meetupUrl = 'https://api.meetup.com/2/open_events.json?and_text=true&:text&:country&:city&:key&:text_format&:order'
-
-//   var searchForEvents = $resource(
-//     meetupUrl,
-//     {
-//       text_format: "text_format=plain",
-//       text: "text=free+beer",
-//       country: "country=gb",
-//       city: "city=" + cityName,
-//       key: 'key=646f252216306e6d712d7c536a3c2565',
-//       order: "order=distance",
-//       callback: 'JSON_CALLBACK'
-//     },
-//     { get: { method: 'JSONP' } }
-//   );
-
-//   return searchForEvents.get().$promise.then(function (response) {
-//     var filteredResults = [];
-//     for (index = 0; index < response.results.length; index++) {
-//       var result = response.results[index].description;
-
-//       try {
-//         result.indexOf('free beer')
-//       }
-//       catch (err) {
-//         $('#error-div').fadeIn(500).delay(8000).fadeOut(1000);
-//         break;
-//       }
-
-//       if (result.indexOf('free beer') >= 0) {
-//         filteredResults.push(response.results[index]);
-//       }
-//     }
-
-//     if (filteredResults.length === 0) {
-//       $('.results').hide();
-//       $('#no-results-div').fadeIn(500).delay(5000).fadeOut(1000);
-//       return
-//     } else {
-//       $('.results').show();
-//       return filteredResults
-//     }
-//   });
-// },
