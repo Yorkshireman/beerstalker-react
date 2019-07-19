4d26efb960890bb21b939286d4be49a682c16ae1
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2019-07-19 11:24:05 +0100

    Delete empty css file

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import FormAndEventsWrapper from './containers/FormAndEventsWrapper';
-import './containers/styles/FormAndEventsWrapper.css';
 
 class App extends Component {
   render() {
